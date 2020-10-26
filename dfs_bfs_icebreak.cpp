6699a3dc5a2d9e9e25cf700452238a07d7b795db
kimjungchun <64156407+kimjungchun@users.noreply.github.com>
2020-10-26 15:48:12 +0900

    dfs_bfs_practice

---
+++ dfs_bfs_icebreak.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+int N, M;
+int graph[1000][1000];
+
+bool dfs(int x, int y)
+{
+	if (x <= -1 || x >= N || y <= -1 || y >= M) {
+		return false;
+	}
+	if (graph[x][y] == 0)
+	{
+		graph[x][y] = 1;
+		dfs(x - 1, y);
+		dfs(x , y-1);
+		dfs(x , y+1);
+		dfs(x + 1, y);
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+int main()
+{
+	
+	cin >> N >> M;
+	
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < M; j++)
+		{
+			cin >> graph[i][j];
+		}
+	}
+
+	int result = 0;
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+			if (dfs(i, j)) {
+				result += 1;
+			}
+		}
+	}
+
+	cout << result;
+}
