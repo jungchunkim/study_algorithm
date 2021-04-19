f8be4b688f6df2b70a6b4287f59ee355e0c4ec6e
kimjungchun <wjdcjs22@naver.com>
2021-04-19 22:36:48 +0900

    이것이 취업을 위한 코딩테스트다. 8. 다이나믹 프로그래밍 - 1로 만들기

---
+++ dynamic_programming/DP_1.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int d[30001];
+
+int main(void) 
+{
+	int X;
+	cin >> X;
+
+
+	for (int i = 2; i <= X; i++)
+	{
+		// 1À» »« °æ¿ì
+		d[i] = d[i - 1] + 1;
+
+		if (i % 2 == 0)
+		{
+			d[i] = min(d[i], d[i / 2] + 1);
+		}
+
+		if (i % 3 == 0)
+		{
+			d[i] = min(d[i], d[i / 3] + 1);
+		}
+
+		if (i % 5 == 0)
+		{
+			d[i] = min(d[i], d[i / 5] + 1);
+		}
+	}
+	cout << d[X];
+}
