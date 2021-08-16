45bb3875551fcf99a22232cbbe6f24de5a286d8e
jungchunkim <wjdcjs22@naver.com>
2021-08-16 13:53:10 +0900

    [review] 4-4(1)

--- [NDB_Coding_test]review/4. implementation/4-4.cpp
+++ [NDB_Coding_test]review/4. implementation/4-4.cpp
@@ -2,20 +2,20 @@
 
 using namespace std;
 
+
+// 전체 맵 정보
 int arr[50][50];
 
+//북,동,남,서
 int dx[] = { 0,1,0,-1 };
 int dy[] = { -1,0,1,0 };
 
 int main()
 {
 	int N, M;
-
 	cin >> N >> M;
-
-	int count = 0;	// 방문한 칸의 수
-
 	int x, y, dir;
+	int count = 0;	// 방문한 칸의 수
 	cin >> x >> y >> dir;	// 캐릭터 시작 위치, 방향
 
 	for (int i = 0; i < N; i++)
@@ -41,7 +41,8 @@ int main()
 		{
 			temp--;
 		}
-
+		
+		int k = 0;
 		while (temp != dir)
 		{// 같은 방향 나오기 전까지
 			int x2 = x + dx[temp];
@@ -54,7 +55,8 @@ int main()
 				x = x2;
 				y = y2;
 				count++;
-
+				dir = temp;
+				k = 1;
 				break;
 			}
 			if (temp - 1 < 0)
@@ -67,12 +69,12 @@ int main()
 			}
 		}
 
-		if (temp == dir)
+		if (temp == dir && k == 0)
 		{
 			int x2 = x + dx[temp];
 			int y2 = y + dy[temp];
 
-			if (arr[x2][y2] == 0)
+			if (arr[x2][y2] == 0 )
 			{//갈 수 있는 경우
 				arr[x2][y2] = 2;
 				x = x2;
@@ -101,7 +103,6 @@ int main()
 				}
 			}
 		}
-		
 	}
 
 	cout << count;
