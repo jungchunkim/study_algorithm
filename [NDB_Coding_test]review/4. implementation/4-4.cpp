0bb0c3a0a66d2e5d9038c19af71a0379051d1ae4
jungchunkim <wjdcjs22@naver.com>
2021-08-16 13:28:31 +0900

    [review] 4-4

---
+++ [NDB_Coding_test]review/4. implementation/4-4.cpp
@@ -0,0 +1,109 @@
+#include <iostream>
+
+using namespace std;
+
+int arr[50][50];
+
+int dx[] = { 0,1,0,-1 };
+int dy[] = { -1,0,1,0 };
+
+int main()
+{
+	int N, M;
+
+	cin >> N >> M;
+
+	int count = 0;	// 방문한 칸의 수
+
+	int x, y, dir;
+	cin >> x >> y >> dir;	// 캐릭터 시작 위치, 방향
+
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < M; j++)
+		{
+			cin >> arr[i][j];
+		}
+	}
+
+	arr[x][y] = 2;
+	count++;
+
+	while (true)
+	{
+		// 방향 정하기
+		int temp = dir;
+		if (temp - 1 < 0)
+		{
+			temp = 3;
+		}
+		else
+		{
+			temp--;
+		}
+
+		while (temp != dir)
+		{// 같은 방향 나오기 전까지
+			int x2 = x + dx[temp];
+			int y2 = y + dy[temp];
+			
+			// 갈 수 있을 경우
+			if (arr[x2][y2] == 0)
+			{
+				arr[x2][y2] = 2;
+				x = x2;
+				y = y2;
+				count++;
+
+				break;
+			}
+			if (temp - 1 < 0)
+			{
+				temp = 3;
+			}
+			else
+			{
+				temp--;
+			}
+		}
+
+		if (temp == dir)
+		{
+			int x2 = x + dx[temp];
+			int y2 = y + dy[temp];
+
+			if (arr[x2][y2] == 0)
+			{//갈 수 있는 경우
+				arr[x2][y2] = 2;
+				x = x2;
+				y = y2;
+				count++;
+			}
+			else
+			{//갈수 없는 경우 뒤로 가야됨
+				temp--;
+				if (temp < 0)
+				{
+					temp = 3;
+				}
+				temp--;
+				if (temp < 0)
+				{
+					temp = 3;
+				}
+				
+				x = x + dx[temp];
+				y = y + dy[temp];
+
+				if (arr[x][y] == 1)
+				{
+					break;
+				}
+			}
+		}
+		
+	}
+
+	cout << count;
+
+}
