9099e9dd76130376ca33f3f83820a901d44dff7f
jungchunkim <wjdcjs22@naver.com>
2021-08-18 19:34:10 +0900

    [review] 6-3

---
+++ [NDB_Coding_test]review/6. sorting/6-3.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+
+int main()
+{
+	int N, K;
+	cin >> N >> K;
+	vector<int> arr;
+	for (int i = 0; i < N; i++)
+	{
+		int num;
+		cin >> num;
+		arr.push_back(num);
+	}
+	
+	vector<int> brr;
+	for (int i = 0; i < N; i++)
+	{
+		int num;
+		cin >> num;
+		brr.push_back(num);
+	}
+
+	sort(arr.begin(), arr.end());
+	sort(brr.begin(), brr.end());
+
+	for (int i = 0; i < K; i++)
+	{
+		if (arr[0 + i] < brr[(N - 1) - i])
+		{
+			arr[0 + i] = brr[(N - 1) - i];
+		}
+	}
+	int sum = 0;
+	for (int i = 0; i < N; i++)
+	{
+		sum += arr[i];
+	}
+	cout << sum;
+}
